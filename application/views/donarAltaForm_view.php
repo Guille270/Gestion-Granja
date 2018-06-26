ca8a462caf624c7e0b942334d1f77ee500cb7ae4
Guille270 <35235586+Guille270@users.noreply.github.com>
2018-06-26 17:47:34 +0200

    Update donarAltaForm_view.php
    Parar ctrl + J lector Codigo de barras

--- application/views/donarAltaForm_view.php
+++ application/views/donarAltaForm_view.php
@@ -61,6 +61,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             }
 
         </script>
+		
+		<script>
+  			document.addEventListener('keydown', function(event) {
+   			 if( event.keyCode == 13 || event.keyCode == 17 || event.keyCode == 74 ) {
+      			event.preventDefault();
+			 }
+  });
+</script>
     </head>
 
     <body onload="calendariAvui()">
